4cb01ad9db84a0db14276b377563a0d50ae1cb9e
ignacio M <ignafree97@gmail.com>
2026-08-12 16:40:06 -0300

    [Feat]: Disabled project sections

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <About />
         <Skills />
         <Experience />
-        <Projects />
+        {/* <Projects /> */}
         <Education />
         <Contact />
       </main>
